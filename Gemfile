04c9cf5870d7070dbf0152330d34b7e80ce17425
Lucas Nicodemus <shank@shanked.me>
2013-08-03 18:42:50 -0600

    Added Gemfile Added more functionality

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+gem 'sinatra', '1.4.3'
+gem 'sinatra-session', '1.0.0'
+gem 'mysql2', '0.3.13'
+gem 'rerun', '0.8.1'
+gem "formatador", "~> 0.2.4"
---
+++ helpers/database.rb
@@ -0,0 +1,64 @@
+class WarningDatabase
+  def initialize(username, password, hostname, port)
+    @username = username
+    @password = password
+    @hostname = hostname
+    @port = port
+  end
+
+  def connect
+    @sql_client = Mysql2::Client.new(:host => @hostname,
+    :username => @username,
+    :password => @password,
+    :port => @port,
+    :cast_booleans => true,
+    :database => 'shinda')
+  end
+
+  def has_warning?(pin)
+    results = @sql_client.query("SELECT * FROM warnings WHERE pin='#{@sql_client.escape(pin)}'")
+    puts results
+    if results.count > 0 then
+      results.each do |row|
+        if (row[:ack] == 0) then
+          return true
+        else
+          return false
+        end
+      end
+    else
+      return false
+    end
+  end
+
+  def warning_exists?(id)
+    results = @sql_client.query("SELECT * FROM warnings WHERE id=#{id}")
+    if results.count > 0 then
+      results.each do |row|
+        if (!row[:ack]) then
+          return false
+        else
+          return true
+        end
+      end
+    else
+      return false
+    end
+  end
+
+  def get_warning(pin)
+    results = @sql_client.query("SELECT * FROM warnings WHERE pin='#{@sql_client.escape(pin)} LIMIT 1'")
+    results.each do |row|
+      return row
+    end
+  end
+
+  def ack_warning?(id)
+    @sql_client.query("UPDATE warnings SET ack=1, pin=null WHERE id=#{id}")
+    if (warning_exists?(id)) then
+      return false
+    else
+      return true
+    end
+  end
+end
