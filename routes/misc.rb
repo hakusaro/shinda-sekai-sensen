88c112c8f9fecf2b97694bb13c0beedd3e33303a
Lucas Nicodemus <shank@shanked.me>
2013-08-11 11:35:54 -0600

    Added test warning route

--- routes/misc.rb
+++ routes/misc.rb
@@ -6,10 +6,10 @@ get '/' do
   output
 end
 
-get '/test/test/?' do
+get '/test/?' do
   output = ""
   output << @header
-  output << partial(:generic, :locals => { title: 'This is a title', subhead: '', message: 'Lorem ipsum here.', link: '/', link_text: 'Return Home'})
+  output << partial(:warning)
   output << partial(:footer)
   output
 end
