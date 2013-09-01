a5a48e5cf87a5251e992dd56c952def82ae056d5
Lucas Nicodemus <shank@shanked.me>
2013-09-01 17:41:31 -0600

    Update Gemfile and app.rb to include Heroku info.

--- app.rb
+++ app.rb
@@ -44,6 +44,15 @@ else
   set :port => 4567
 end
 
+unless (app_name = ENV["HEROKU_APP_NAME"]).nil?
+  require 'heroku-api'
+
+  heroku  = Heroku::API.new(:api_key => ENV["HEROKU_API_KEY"])
+  release = heroku.get_releases(app_name).body.last
+
+  ENV["HEROKU_RELEASE_NAME"] = release["name"]
+end
+
 set :logging => true,
   :dump_errors => true,
   :raise_errors => true,
